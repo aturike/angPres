599a0eb4ecb31fe1dd0fa657860c1dc22071ca5a
Adam Turi <adam.turi@vattenfall.com>
2024-03-16 11:14:59 +0100

    add form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,8 +12,10 @@ import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { en_US } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
 
 registerLocaleData(en);
 
@@ -27,11 +29,14 @@ registerLocaleData(en);
     BrowserAnimationsModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
+    NzFormModule,
+    NzInputModule,
   ],
   providers: [
     { provide: NZ_I18N, useValue: en_US },
     provideAnimationsAsync(),
-    provideHttpClient()
+    provideHttpClient(),
   ],
   bootstrap: [AppComponent],
 })
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+interface IEmailFormConfig {
+  email: string;
+}
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  emailForm: FormGroup;
 
+  formConfig: IEmailFormConfig = {
+    email: 'email',
+  };
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm() {
+    this.emailForm = new FormGroup({
+      [this.formConfig.email]: new FormControl('', [
+        Validators.required,
+        Validators.email,
+      ]),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.emailForm.value);
+  }
 }
